706c21942b1eac87fa71f674a5fbcbee156b1fbc
sanjusss <sanjusss@qq.com>
2019-04-04 10:46:40 +0800

    完成第26题

---
+++ 0026/solution.hpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    int removeDuplicates(vector<int> &nums) 
+    {
+        int index = 0;
+        int last = INT_MIN;
+        for (int i : nums)
+        {
+            if (i != last)
+            {
+                last = i;
+                nums[index] = i;
+                ++index;
+            }
+        }
+
+        return index;
+    }
+};
