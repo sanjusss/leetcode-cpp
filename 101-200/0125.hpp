0bc07105398946b09608a0804119123aa03501ac
sanjusss <sanjusss@qq.com>
2019-04-06 22:36:10 +0800

    完成125

---
+++ 101-200/0125.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "leetcode.h"
+
+class Solution 
+{
+public:
+    bool isPalindrome(string s) 
+    {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end)
+        {
+            if (!isalnum(s[start]))
+            {
+                ++start;
+                continue;
+            }
+
+            if (!isalnum(s[end]))
+            {
+                --end;
+                continue;
+            }
+
+            if (tolower(s[start++]) != tolower(s[end--]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
