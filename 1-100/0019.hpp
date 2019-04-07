2692f6d3f491d63b811a40e318be552ab3223c3a
sanjusss <sanjusss@qq.com>
2019-04-07 15:01:21 +0800

    完成19

---
+++ 1-100/0019.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#pragma warning(disable:28182)
+#include "leetcode.h"
+
+class Solution 
+{
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) 
+    {
+        shared_ptr<ListNode> root(new ListNode(0));
+        root->next = head;
+        auto second = root.get();
+        for (int i = 0; i < n; ++i)
+        {
+            head = head->next;
+        }
+
+        while (head != nullptr)
+        {
+            head = head->next;
+            second = second->next;
+        }
+
+        shared_ptr<ListNode> removed(second->next);
+        second->next = second->next->next;
+        return root->next;
+    }
+};
