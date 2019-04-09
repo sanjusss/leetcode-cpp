0f0a12430ada818bdde992de4e395f1ddc39da3c
sanjusss <sanjusss@qq.com>
2019-04-09 10:35:12 +0800

    完成190

---
+++ 101-200/0190.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "leetcode.h"
+
+class Solution 
+{
+public:
+    uint32_t reverseBits(uint32_t n) 
+    {
+        uint32_t res = 0;
+        for (int i = 0; i < 32; ++i)
+        {
+            res <<= 1;
+            res += (n >> i) & 1;
+        }
+
+        return res;
+    }
+};
