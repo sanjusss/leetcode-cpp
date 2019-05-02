25f58cf9f9f0d53279c58b60d96b3e2dd2ecb052
陈玉鹏 <sanjusss@qq.com>
2019-05-02 09:08:20 +0800

    增加350的哈希映射方法。

--- 301-400/0350.hpp
+++ 301-400/0350.hpp
@@ -3,33 +3,60 @@
 class Solution 
 {
 public:
+    //使用哈希映射
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
     {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-        list<int> res;
-        int i = 0;
-        int j = 0;
-        while (i < nums1.size() && j < nums2.size())
+        vector<int> res;
+        unordered_multiset<int> vs;
+        vector<int>& a = nums1.size() > nums2.size() ? nums2 : nums1;
+        vector<int>& b = nums1.size() > nums2.size() ? nums1 : nums2;
+
+        for (auto i : a)
         {
-            if (nums1[i] == nums2[j])
+            vs.insert(i);
+        }
+
+        for (auto i : b)
+        {
+            auto f = vs.find(i);
+            if (f != vs.end())
             {
-                res.push_back(nums1[i]);
-                ++i;
-                ++j;
-            }
-            else if (nums1[i] > nums2[j])
-            {
-                ++j;
-            }
-            else
-            {
-                ++i;
+                res.push_back(i);
+                vs.erase(f);
             }
         }
 
-        return vector<int>(res.begin(), res.end());
+        return res;
     }
+
+    //排序后比较
+    //vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
+    //{
+    //    sort(nums1.begin(), nums1.end());
+    //    sort(nums2.begin(), nums2.end());
+    //    list<int> res;
+    //    int i = 0;
+    //    int j = 0;
+    //    while (i < nums1.size() && j < nums2.size())
+    //    {
+    //        if (nums1[i] == nums2[j])
+    //        {
+    //            res.push_back(nums1[i]);
+    //            ++i;
+    //            ++j;
+    //        }
+    //        else if (nums1[i] > nums2[j])
+    //        {
+    //            ++j;
+    //        }
+    //        else
+    //        {
+    //            ++i;
+    //        }
+    //    }
+
+    //    return vector<int>(res.begin(), res.end());
+    //}
 
     //vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
     //{
