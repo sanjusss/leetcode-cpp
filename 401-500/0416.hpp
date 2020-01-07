16ff956eae6143402aade0a8b56e8d07988ae441
sanjusss <sanjusss@qq.com>
2020-01-07 19:31:34 +0800

    增加416边界容错。

--- 401-500/0416.hpp
+++ 401-500/0416.hpp
@@ -42,6 +42,7 @@ public:
 
         sum /= 2;
         vector<vector<bool>> dp(nums.size() + 1, vector<bool>(sum + 1, false));
+        dp[0][0] = true;
         for (int i = 1; i <= nums.size(); ++i)
         {
             for (int j = nums[i - 1]; j <= sum; ++j)
