f34bece75fb8a6e5c8ac5a42544dfccaf2e652b7
sanjusss <sanjusss@qq.com>
2020-01-07 15:53:38 +0800

    临时保存。

---
+++ UnitTestLCP/UnitTestLCP4.cpp
@@ -0,0 +1,51 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LCP/LCP4.hpp"
+#include "unittest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLCP
+{
+	TEST_CLASS(UnitTestLCP4)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			check(2, 3,
+				"[[1, 0], [1, 1]]",
+				2);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			check(2, 3,
+				"[]",
+				3);
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			check(3, 3,
+				"[]",
+				4);
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			check(4, 4,
+				"[[1, 1], [2, 2]]",
+				6);
+		}
+
+	private:
+		void check(int n, int m, string brokenSrc, int expected)
+		{
+			auto broken = toInt2DArray(brokenSrc);
+			Solution sln;
+			auto actual = sln.domino(n, m, broken);
+			Assert::AreEqual(expected, actual);
+		}
+	};
+}
