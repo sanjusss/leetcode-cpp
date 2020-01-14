1e2b33d0244fc23bb009e6d5efd1be52b5ecd42b
sanjusss <sanjusss@qq.com>
2020-01-14 14:34:03 +0800

    完成106，修复105和106测试用例之间的冲突。

--- 101-200/0106.hpp
+++ 101-200/0106.hpp
@@ -1,27 +1,27 @@
 #pragma once
 #include "leetcode.h"
 
-namespace Code0105
+namespace Code0106
 {
 
     class Solution
     {
     public:
-        TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+        TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder)
         {
-            return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
+            return buildTree(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1);
         }
 
     private:
-        TreeNode* buildTree(const vector<int>& preorder, int pStart, int pEnd, const vector<int>& inorder, int iStart, int iEnd)
+        TreeNode* buildTree(const vector<int>& inorder, int iStart, int iEnd, const vector<int>& postorder, int pStart, int pEnd)
         {
             if (pStart > pEnd)
             {
                 return nullptr;
             }
 
+            int val = postorder[pEnd];
             int mid = -1;
-            int val = preorder[pStart];
             for (int i = iStart; i <= iEnd; ++i)
             {
                 if (inorder[i] == val)
@@ -37,8 +37,8 @@ namespace Code0105
             }
 
             TreeNode* node = new TreeNode(val);
-            node->left = buildTree(preorder, pStart + 1, pStart + mid - iStart, inorder, iStart, mid - 1);
-            node->right = buildTree(preorder, pStart + mid - iStart + 1, pEnd, inorder, mid + 1, iEnd);
+            node->left = buildTree(inorder, iStart, mid - 1, postorder, pStart, pStart + mid - iStart - 1);
+            node->right = buildTree(inorder, mid + 1, iEnd, postorder, pStart + mid - iStart, pEnd - 1);
             return node;
         }
     };
--- UnitTest101-200/UnitTest106.cpp
+++ UnitTest101-200/UnitTest106.cpp
@@ -1,28 +1,28 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../101-200/0105.hpp"
+#include "../101-200/0106.hpp"
 #include "unittest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest101200
 {
-    TEST_CLASS(UnitTest105)
+    TEST_CLASS(UnitTest106)
     {
     public:
 
         TEST_METHOD(TestMethod1)
         {
-            check("[3,9,20,15,7]", "[9,3,15,20,7]", "[3, 9, 20, null, null, 15, 7]");
+            check("[9,3,15,20,7]", "[9,15,7,20,3]", "[3,9,20,null,null,15,7]");
         }
 
-        void check(string preorderSrc, string inorderSrc, string expectedSrc)
+        void check(string inorderSrc, string postorderSrc, string expectedSrc)
         {
-            auto preorder = toIntArray(preorderSrc);
             auto inorder = toIntArray(inorderSrc);
+            auto postorder = toIntArray(postorderSrc);
             auto expected = toTreeNode(expectedSrc);
-            Code0105::Solution sln;
-            auto actual = sln.buildTree(preorder, inorder);
+            Code0106::Solution sln;
+            auto actual = sln.buildTree(inorder, postorder);
             Assert::AreEqual(*expected, *actual);
             freeTreeNode(expected);
             freeTreeNode(actual);
