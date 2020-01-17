ad5f3b2b2f0cb0720f2feb3e8c4e60b2e837d729
sanjusss <sanjusss@qq.com>
2020-01-17 15:39:24 +0800

    完成第47题。

---
+++ UnitTest1-100/UnitTest47.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../1-100/0047.hpp"
+#include "unittest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1100
+{
+    TEST_CLASS(UnitTest0047)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            check("[1,2,3]", "[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]");
+        }
+
+        void check(string numsSrc, string expectedSrc)
+        {
+            auto nums = toIntArray(numsSrc);
+            auto expected = toInt2DArray(expectedSrc);
+            Solution sln;
+            auto actual = sln.permute(nums);
+            Assert::AreEqual(expected, actual);
+        }
+    };
+}
