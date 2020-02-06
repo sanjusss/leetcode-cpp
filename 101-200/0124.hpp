ab239864f98dd203760ce4e9d749c6821c751e81
sanjusss <sanjusss@qq.com>
2020-02-06 10:35:38 +0800

    优化124

--- 101-200/0124.hpp
+++ 101-200/0124.hpp
@@ -58,31 +58,58 @@
 //    }
 //};
 
+////递归列举所有情况
+//class Solution
+//{
+//public:
+//    int maxPathSum(TreeNode* root)
+//    {
+//        return find(root).first;
+//    }
+//
+//private:
+//    /*
+//    返回值
+//    first：最大值
+//    second：包含node节点的最大路径，不能同时包含左右节点，可以只包括node节点。
+//    */
+//    pair<int, int> find(TreeNode* node)
+//    {
+//        if (node == nullptr)
+//        {
+//            return { INT_MIN >> 2, INT_MIN >> 2};
+//        }
+//
+//        auto lp = find(node->left);
+//        auto rp = find(node->right);
+//        int line = max({ node->val, node->val + lp.second, node->val + rp.second });
+//        int value = max({ line, node->val + lp.second + rp.second, lp.first, rp.first });
+//        return { value, line };
+//    }
+//};
+
+//后续遍历，中途值参与最值比较。
 class Solution
 {
 public:
     int maxPathSum(TreeNode* root)
     {
-        return find(root).first;
-    }
-
-private:
-    /*
-    返回值
-    first：最大值
-    second：包含node节点的最大路径，不能同时包含左右节点，可以只包括node节点。
-    */
-    pair<int, int> find(TreeNode* node)
-    {
-        if (node == nullptr)
+        int ans = INT_MIN;
+        //返回值：包含node节点的最大路径，不能同时包含左右节点，可以只包括node节点。
+        auto find = [&ans](auto&& self, TreeNode* node)
         {
-            return { INT_MIN >> 2, INT_MIN >> 2};
-        }
+            if (node == nullptr)
+            {
+                return 0;
+            }
 
-        auto lp = find(node->left);
-        auto rp = find(node->right);
-        int line = max({ node->val, node->val + lp.second, node->val + rp.second });
-        int value = max({ line, node->val + lp.second + rp.second, lp.first, rp.first });
-        return { value, line };
+            int left = max(0, self(self, node->left));
+            int right = max(0, self(self, node->right));
+            ans = max(ans, node->val + left + right);
+            return max(node->val + left, node->val + right);
+        };
+
+        find(find, root);
+        return ans;
     }
 };
