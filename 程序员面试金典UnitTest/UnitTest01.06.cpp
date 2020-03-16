80991231bdd4be19ae226fa69015b8dad4944f62
sanjusss <sanjusss@qq.com>
2020-03-16 19:52:55 +0800

    完成01.06

---
+++ 程序员面试金典UnitTest/UnitTest01.06.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "unittest.h"
+#include "../程序员面试金典/01.06.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace 程序员面试金典UnitTest01
+{
+	TEST_CLASS(程序员面试金典UnitTest01_06)
+	{
+	public:
+        TEST_METHOD(TestMethod1)
+        {
+            check("aabcccccaaa",
+                "a2b1c5a3");
+        }
+
+        TEST_METHOD(TestMethod2)
+        {
+            check("abbccd",
+                "abbccd");
+        }
+
+        TEST_METHOD(TestMethod3)
+        {
+            check("bb",
+                "bb");
+        }
+
+        void check(string S, string expected)
+        {
+            Solution sln;
+            auto actual = sln.compressString(S);
+            Assert::AreEqual(expected, actual);
+        }
+	};
+}
