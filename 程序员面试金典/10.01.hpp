0cea4c710638b57d63fd2c6bca3f4e7c6407024a
sanjusss <sanjusss@qq.com>
2020-03-23 13:11:27 +0800

    完成10.01

--- 程序员面试金典/10.01.hpp
+++ 程序员面试金典/10.01.hpp
@@ -1,22 +1,42 @@
 #pragma once
 #include "leetcode.h"
 
-class Solution 
+//class Solution 
+//{
+//public:
+//    void merge(vector<int>& A, int m, vector<int>& B, int n) 
+//    {
+//        auto AEnd = next(A.begin(), m - 1);
+//        for (auto i : B)
+//        {
+//            ++AEnd;
+//            auto pos = upper_bound(A.begin(), AEnd, i);
+//            for (auto j = AEnd; j != pos; --j)
+//            {
+//                *j = *prev(j);
+//            }
+//
+//            *pos = i;
+//        }
+//    }
+//};
+
+class Solution
 {
 public:
-    void merge(vector<int>& A, int m, vector<int>& B, int n) 
+    void merge(vector<int>& A, int m, vector<int>& B, int n)
     {
-        auto AEnd = next(A.begin(), m - 1);
-        for (auto i : B)
+        auto a = next(A.rbegin(), n);
+        auto b = B.rbegin();
+        auto cur = A.rbegin();
+        while (a != A.rend() && b != B.rend())
         {
-            ++AEnd;
-            auto pos = upper_bound(A.begin(), AEnd, i);
-            for (auto j = AEnd; j != pos; --j)
-            {
-                *j = *prev(j);
-            }
+            *(cur++) = (*a > *b) ? *(a++) : *(b++);
+        }
 
-            *pos = i;
+        while (b != B.rend())
+        {
+            *(cur++) = *(b++);
         }
     }
 };
