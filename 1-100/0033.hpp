b8069a4ea508bca6e5c93214b7fadae1a587f7df
sanjusss <sanjusss@qq.com>
2020-04-27 09:00:48 +0800

    完成33题的另外一种解法。

--- 1-100/0033.hpp
+++ 1-100/0033.hpp
@@ -1,62 +1,125 @@
 #pragma once
 #include "leetcode.h"
 
-class Solution 
+//class Solution 
+//{
+//public:
+//    int search(vector<int>& nums, int target) 
+//    {
+//        int div = findDiv(nums);
+//        int res = search(nums, target, 0, div);
+//        if (res == -1)
+//        {
+//            res = search(nums, target, div + 1, nums.size() - 1);
+//        }
+//
+//        return res;
+//    }
+//
+//private:
+//    int search(vector<int>& nums, int target, int left, int right)
+//    {
+//        while (left <= right)
+//        {
+//            int mid = (left + right) / 2;
+//            if (nums[mid] == target)
+//            {
+//                return mid;
+//            }
+//            else if (nums[mid] > target)
+//            {
+//                right = mid - 1;
+//            }
+//            else
+//            {
+//                left = mid + 1;
+//            }
+//        }
+//
+//        return -1;
+//    }
+//
+//    int findDiv(vector<int>& nums)
+//    {
+//        int left = 0;
+//        int right = nums.size() - 2;
+//        while (left <= right)
+//        {
+//            int mid = (left + right) / 2;
+//            if (nums[mid] > nums[mid + 1])
+//            {
+//                return mid;
+//            }
+//            else if (nums[mid] < nums[0])
+//            {
+//                right = mid - 1;
+//            }
+//            else
+//            {
+//                left = mid + 1;
+//            }
+//        }
+//
+//        return -1;
+//    }
+//};
+
+class Solution
 {
 public:
-    int search(vector<int>& nums, int target) 
+    int search(vector<int>& nums, int target)
     {
-        int div = findDiv(nums);
-        int res = search(nums, target, 0, div);
-        if (res == -1)
+        if (nums.empty())
         {
-            res = search(nums, target, div + 1, nums.size() - 1);
+            return -1;
         }
 
-        return res;
-    }
-
-private:
-    int search(vector<int>& nums, int target, int left, int right)
-    {
-        while (left <= right)
+        int start = nums.front();
+        int end = nums.back();
+        if (target > end && target < start)
         {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target)
-            {
-                return mid;
-            }
-            else if (nums[mid] > target)
-            {
-                right = mid - 1;
-            }
-            else
-            {
-                left = mid + 1;
-            }
+            return -1;
         }
 
-        return -1;
-    }
-
-    int findDiv(vector<int>& nums)
-    {
         int left = 0;
-        int right = nums.size() - 2;
-        while (left <= right)
+        int right = nums.size() - 1;
+        int mid;
+        if (target <= end)
         {
-            int mid = (left + right) / 2;
-            if (nums[mid] > nums[mid + 1])
+            while (left <= right)
             {
-                return mid;
+                mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] > end || nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
             }
-            else if (nums[mid] < nums[0])
+        }
+        else
+        {
+            while (left <= right)
             {
-                right = mid - 1;
-            }
-            else
-            {
-                left = mid + 1;
+                mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < end || nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
             }
         }
 
