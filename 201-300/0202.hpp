c4ac0ba9aa21f7baf3be6b7e8e0227e377b08e6a
sanjusss <sanjusss@qq.com>
2020-04-30 09:04:01 +0800

    增加202的快慢指针方法。

--- 201-300/0202.hpp
+++ 201-300/0202.hpp
@@ -1,41 +1,77 @@
 #pragma once
 #include "leetcode.h"
 
-class Solution 
+//class Solution 
+//{
+//public:
+//    bool isHappy(int n) 
+//    {
+//        unordered_set<int> used;
+//        int sum;
+//        int temp;
+//        while (true)
+//        {
+//            if (n == 1)
+//            {
+//                return true;
+//            }
+//
+//            if (used.count(n) > 0)
+//            {
+//                return false;
+//            }
+//            else
+//            {
+//                used.insert(n);
+//            }
+//
+//            sum = 0;
+//            while (n != 0)
+//            {
+//                temp = n % 10;
+//                sum += temp * temp;
+//                n /= 10;
+//            }
+//
+//            n = sum;
+//        }
+//
+//        return false;
+//    }
+//};
+
+class Solution
 {
 public:
-    bool isHappy(int n) 
+    bool isHappy(int n)
     {
-        unordered_set<int> used;
-        int sum;
-        int temp;
-        while (true)
+        if (n == 1)
         {
-            if (n == 1)
-            {
-                return true;
-            }
-
-            if (used.count(n) > 0)
-            {
-                return false;
-            }
-            else
-            {
-                used.insert(n);
-            }
-
-            sum = 0;
-            while (n != 0)
-            {
-                temp = n % 10;
-                sum += temp * temp;
-                n /= 10;
-            }
-
-            n = sum;
+            return true;
         }
 
-        return false;
+        int slow = n;
+        int fast = n;
+        do
+        {
+            slow = next(slow);
+            fast = next(next(fast));
+        } while (slow != 1 && slow != fast);
+        return slow == 1;
+    }
+
+private:
+    int next(int n)
+    {
+        int sum = 0;
+        int temp;
+        while (n > 0)
+        {
+            temp = n % 10;
+            sum += temp * temp;
+            n /= 10;
+        }
+
+        return sum;
     }
 };
