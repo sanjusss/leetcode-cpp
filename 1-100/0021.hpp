864ad2216201f15be412a07db0025d7e10a85812
sanjusss <sanjusss@qq.com>
2020-05-01 10:02:22 +0800

    改进21的效率。

--- 1-100/0021.hpp
+++ 1-100/0021.hpp
@@ -1,17 +1,44 @@
 #pragma once
 #include "leetcode.h"
 
-class Solution 
+//class Solution 
+//{
+//public:
+//    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+//    {
+//        shared_ptr<ListNode> root(new ListNode(0));
+//        auto head = root.get();
+//        while (l1 != nullptr || l2 != nullptr)
+//        {
+//            if (l2 == nullptr
+//                || (l1 != nullptr && l1->val < l2->val))
+//            {
+//                head->next = l1;
+//                l1 = l1->next;
+//            }
+//            else
+//            {
+//                head->next = l2;
+//                l2 = l2->next;
+//            }
+//
+//            head = head->next;
+//        }
+//
+//        return root->next;
+//    }
+//};
+
+class Solution
 {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
     {
-        shared_ptr<ListNode> root(new ListNode(0));
-        auto head = root.get();
-        while (l1 != nullptr || l2 != nullptr)
+        ListNode root(0);
+        ListNode* head = &root;
+        while (l1 != nullptr && l2 != nullptr)
         {
-            if (l2 == nullptr
-                || (l1 != nullptr && l1->val < l2->val))
+            if (l1->val < l2->val)
             {
                 head->next = l1;
                 l1 = l1->next;
@@ -25,6 +52,15 @@ public:
             head = head->next;
         }
 
-        return root->next;
+        if (l1 != nullptr)
+        {
+            head->next = l1;
+        }
+        else if (l2 != nullptr)
+        {
+            head->next = l2;
+        }
+
+        return root.next;
     }
 };
