71c02284221a69ee43deffd8867300c52dd83bda
sanjusss <sanjusss@qq.com>
2020-05-12 07:38:53 +0800

    优化155

--- 101-200/0155.hpp
+++ 101-200/0155.hpp
@@ -1,38 +1,81 @@
 #pragma once
 #include "leetcode.h"
 
-class MinStack 
+//class MinStack 
+//{
+//public:
+//    /** initialize your data structure here. */
+//    MinStack() 
+//    {
+//
+//    }
+//
+//    void push(int x) 
+//    {
+//        m_set.insert(x);
+//        m_stack.push(x);
+//    }
+//
+//    void pop() 
+//    {
+//        m_set.erase(m_set.find(m_stack.top()));
+//        m_stack.pop();
+//    }
+//
+//    int top() 
+//    {
+//        return m_stack.top();
+//    }
+//
+//    int getMin() 
+//    {
+//        return *m_set.begin();
+//    }
+//
+//private:
+//    multiset<int> m_set;
+//    stack<int> m_stack;
+//};
+
+class MinStack
 {
 public:
     /** initialize your data structure here. */
-    MinStack() 
+    MinStack()
     {
-
+        m_mins.push(INT_MAX);
     }
 
-    void push(int x) 
+    void push(int x)
     {
-        m_set.insert(x);
         m_stack.push(x);
+        if (m_mins.top() >= x)
+        {
+            m_mins.push(x);
+        }
     }
 
-    void pop() 
+    void pop()
     {
-        m_set.erase(m_set.find(m_stack.top()));
+        if (m_stack.top() == m_mins.top())
+        {
+            m_mins.pop();
+        }
+
         m_stack.pop();
     }
 
-    int top() 
+    int top()
     {
         return m_stack.top();
     }
 
-    int getMin() 
+    int getMin()
     {
-        return *m_set.begin();
+        return m_mins.top();
     }
 
 private:
-    multiset<int> m_set;
+    stack<int> m_mins;
     stack<int> m_stack;
 };
