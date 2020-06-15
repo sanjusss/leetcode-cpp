4184b02fe5f695b8793acf946da19b9da66c9ad7
sanjusss <sanjusss@qq.com>
2020-06-15 16:54:56 +0800

    改进14题。

--- 1-100/0014.hpp
+++ 1-100/0014.hpp
@@ -1,37 +1,75 @@
 #pragma once
 #include "leetcode.h"
 
-class Solution 
+//class Solution 
+//{
+//public:
+//    string longestCommonPrefix(vector<string>& strs) 
+//    {
+//        if (strs.size() == 0)
+//        {
+//            return string();
+//        }
+//
+//        int length = INT_MAX;
+//        for (auto& s : strs)
+//        {
+//            length = min((int)s.length(), length);
+//        }
+//
+//        string res;
+//        int strCount = strs.size();
+//        for (int i = 0; i < length; ++i)
+//        {
+//            for (int j = 1; j < strCount; ++j)
+//            {
+//                if (strs[0][i] != strs[j][i])
+//                {
+//                    return res;
+//                }
+//            }
+//
+//            res += strs[0][i];
+//        }
+//
+//        return res;
+//    }
+//};
+
+class Solution
 {
 public:
-    string longestCommonPrefix(vector<string>& strs) 
+    string longestCommonPrefix(vector<string>& strs)
     {
-        if (strs.size() == 0)
+        if (strs.empty())
         {
             return string();
         }
 
-        int length = INT_MAX;
-        for (auto& s : strs)
+        int length = 0;
+        string& t = strs.front();
+        bool success = true;
+        while (length < t.size())
         {
-            length = min((int)s.length(), length);
-        }
-
-        string res;
-        int strCount = strs.size();
-        for (int i = 0; i < length; ++i)
-        {
-            for (int j = 1; j < strCount; ++j)
+            for (int i = 1; i < strs.size(); ++i)
             {
-                if (strs[0][i] != strs[j][i])
+                if (strs[i].size() <= length || strs[i][length] != t[length])
                 {
-                    return res;
+                    success = false;
+                    break;
                 }
             }
 
-            res += strs[0][i];
+            if (success)
+            {
+                ++length;
+            }
+            else
+            {
+                break;
+            }
         }
 
-        return res;
+        return t.substr(0, length);
     }
 };
