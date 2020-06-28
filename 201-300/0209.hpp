a6bb0782730051c8752a1363b0da0826d8256c17
sanjusss <sanjusss@qq.com>
2020-06-28 09:27:42 +0800

    完善209

--- 201-300/0209.hpp
+++ 201-300/0209.hpp
@@ -1,36 +1,114 @@
 #pragma once
 #include "leetcode.h"
 
-class Solution 
+//class Solution 
+//{
+//public:
+//    int minSubArrayLen(int s, vector<int>& nums) 
+//    {
+//        int length = nums.size();
+//        int tempSum = 0;
+//        int ml = INT_MAX;
+//        for (int i = 0, j = 0; i < length; ++i)
+//        {
+//            if (i != 0)
+//            {
+//                tempSum -= nums[i - 1];
+//            }
+//
+//            while (tempSum < s && j < length)
+//            {
+//                tempSum += nums[j++];
+//            }
+//
+//            if (tempSum < s)
+//            {
+//                break;
+//            }
+//            else
+//            {
+//                ml = min(ml, j - i);
+//            }
+//        }
+//
+//        return ml == INT_MAX ? 0 : ml;
+//    }
+//};
+
+//class Solution 
+//{
+//public:
+//    int minSubArrayLen(int s, vector<int>& nums) 
+//    {
+//        int ans = INT_MAX;
+//        int n = nums.size();
+//        for (int i = 0; i < n; ++i)
+//        {
+//            int sum = 0;
+//            for (int j = i; j < n; ++j)
+//            {
+//                sum += nums[j];
+//                if (sum >= s)
+//                {
+//                    ans = min(ans, j - i + 1);
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return ans == INT_MAX ? 0 : ans;
+//    }
+//};
+
+//class Solution
+//{
+//public:
+//    int minSubArrayLen(int s, vector<int>& nums)
+//    {
+//        int ans = INT_MAX;
+//        int n = nums.size();
+//        vector<int> sums(n + 1);
+//        for (int i = 0; i < n; ++i)
+//        {
+//            sums[i + 1] = sums[i] + nums[i];
+//        }
+//
+//        for (int i = 0; i < n; ++i)
+//        {
+//            auto pos = lower_bound(next(sums.begin(), i + 1), sums.end(), s + sums[i]);
+//            if (pos != sums.end())
+//            {
+//                ans = min(ans, (int)distance(sums.begin(), pos) - i);
+//            }
+//        }
+//
+//        return ans == INT_MAX ? 0 : ans;
+//    }
+//};
+
+class Solution
 {
 public:
-    int minSubArrayLen(int s, vector<int>& nums) 
+    int minSubArrayLen(int s, vector<int>& nums)
     {
-        int length = nums.size();
-        int tempSum = 0;
-        int ml = INT_MAX;
-        for (int i = 0, j = 0; i < length; ++i)
+        int n = nums.size();
+        int ans = INT_MAX;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        while (end < n)
         {
-            if (i != 0)
+            sum += nums[end];
+            while (sum >= s)
             {
-                tempSum -= nums[i - 1];
+                ans = min(ans, end - start + 1);
+                sum -= nums[start];
+                ++start;
             }
 
-            while (tempSum < s && j < length)
-            {
-                tempSum += nums[j++];
-            }
-
-            if (tempSum < s)
-            {
-                break;
-            }
-            else
-            {
-                ml = min(ml, j - i);
-            }
+            ++end;
         }
 
-        return ml == INT_MAX ? 0 : ml;
+        return ans == INT_MAX ? 0 : ans;
     }
 };
--- UnitTest201-300/UnitTest209.cpp
+++ UnitTest201-300/UnitTest209.cpp
@@ -16,6 +16,11 @@ namespace UnitTest201300
             doTest(7, "[2,3,1,2,4,3]", 2);
         }
 
+        TEST_METHOD(TestMethod2)
+        {
+            doTest(15, "[5,1,3,5,10,7,4,9,2,8]", 2);
+        }
+
         void doTest(int s, string srcNums, int expected)
         {
             vector<int> nums = toIntArray(srcNums);
