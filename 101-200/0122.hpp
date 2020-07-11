5a5f405ee22c8c56e8e5c50422accd31b7ded4ac
sanjusss <sanjusss@qq.com>
2020-07-11 13:25:39 +0800

    122增加一种方法。

--- 101-200/0122.hpp
+++ 101-200/0122.hpp
@@ -1,34 +1,57 @@
 #include "leetcode.h"
 
-class Solution
-{
+//class Solution
+//{
+//public:
+//    int maxProfit(vector<int>& prices)
+//    {
+//        int bought = INT_MIN;
+//        int count = prices.size();
+//        int maxIndex = count - 1;
+//        int profit = 0;
+//        for (int i = 0; i < count; ++i)
+//        {
+//            if (bought == INT_MIN)
+//            {
+//                if (i < maxIndex && prices[i] < prices[i + 1])
+//                {
+//                    bought = prices[i];
+//                }
+//            }
+//            else
+//            {
+//                if (prices[i] > bought &&
+//                    (i == maxIndex || prices[i] > prices[i + 1]))
+//                {
+//                    profit += prices[i] - bought;
+//                    bought = INT_MIN;
+//                }
+//            }
+//        }
+//
+//        return profit;
+//    }
+//};
+
+class Solution {
 public:
-    int maxProfit(vector<int>& prices)
-    {
-        int bought = INT_MIN;
-        int count = prices.size();
-        int maxIndex = count - 1;
-        int profit = 0;
-        for (int i = 0; i < count; ++i)
-        {
-            if (bought == INT_MIN)
-            {
-                if (i < maxIndex && prices[i] < prices[i + 1])
-                {
-                    bought = prices[i];
-                }
-            }
-            else
-            {
-                if (prices[i] > bought &&
-                    (i == maxIndex || prices[i] > prices[i + 1]))
-                {
-                    profit += prices[i] - bought;
-                    bought = INT_MIN;
-                }
-            }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0) {
+            return 0;
         }
 
-        return profit;
+        int hold = -prices[0];
+        int none = 0;
+        int doHold;
+        int doSell;
+        for (int i = 1; i < prices.size(); ++i) {
+            doHold = max(hold, none - prices[i]);
+            doSell = max(hold + prices[i], none);
+            hold = doHold;
+            none = doSell;
+        }
+
+        return max(hold, none);
     }
 };
