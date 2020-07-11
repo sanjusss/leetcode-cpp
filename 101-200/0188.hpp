266e99e686a5c984c89bcfc56e3291812e680143
sanjusss <sanjusss@qq.com>
2020-07-11 15:27:40 +0800

    降低188内存使用。

--- 101-200/0188.hpp
+++ 101-200/0188.hpp
@@ -31,6 +31,50 @@
 //    }
 //};
 
+//class Solution {
+//public:
+//    int maxProfit(int k, vector<int>& prices) {
+//        if (k <= 0) {
+//            return 0;
+//        }
+//
+//        int n = prices.size();
+//        if (n / 2 <= k) {
+//            return maxProfit(prices);
+//        }
+//
+//        vector<vector<vector<int>>> dp(n, vector<vector<int>>(k + 1, vector<int>(2)));
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = k; j >= 1; --j) {
+//                if (i == 0) {
+//                    dp[i][j][0] = 0;
+//                    dp[i][j][1] = -prices[0];
+//                }
+//                else {
+//                    dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+//                    dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+//                }
+//            }
+//        }
+//
+//        return dp[n - 1][k][0];
+//    }
+//
+//private:
+//    int maxProfit(vector<int>& prices) {
+//        int hold = INT_MIN;
+//        int none = 0;
+//        int prevNone;
+//        for (int i : prices) {
+//            prevNone = none;
+//            none = max(none, hold + i);
+//            hold = max(hold, prevNone - i);
+//        }
+//
+//        return none;
+//    }
+//};
+
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
@@ -43,21 +87,20 @@ public:
             return maxProfit(prices);
         }
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(k + 1, vector<int>(2)));
-        for (int i = 0; i < n; ++i) {
+        vector<vector<vector<int>>> dp(2, vector<vector<int>>(k + 1, vector<int>(2)));
+        for (int j = 0; j <= k; ++j) {
+            dp[0][j][1] = INT_MIN;
+        }
+
+        dp[1][0][1] = INT_MIN;
+        for (int i = 1; i <= n; ++i) {
             for (int j = k; j >= 1; --j) {
-                if (i == 0) {
-                    dp[i][j][0] = 0;
-                    dp[i][j][1] = -prices[0];
-                }
-                else {
-                    dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
-                    dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
-                }
+                dp[i & 1][j][0] = max(dp[(i - 1) & 1][j][0], dp[(i - 1) & 1][j][1] + prices[i - 1]);
+                dp[i & 1][j][1] = max(dp[(i - 1) & 1][j][1], dp[(i - 1) & 1][j - 1][0] - prices[i - 1]);
             }
         }
 
-        return dp[n - 1][k][0];
+        return dp[n & 1][k][0];
     }
 
 private:
