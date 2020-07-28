888dab6b4e133afdce64da684c1edeb4e7588529
sanjusss <sanjusss@qq.com>
2020-07-28 08:18:58 +0800

    104增加迭代方法。

--- 101-200/0104.hpp
+++ 101-200/0104.hpp
@@ -1,18 +1,48 @@
 #pragma once
 #include "leetcode.h"
 
-class Solution 
-{
+//class Solution 
+//{
+//public:
+//    int maxDepth(TreeNode* root) 
+//    {
+//        if (root == nullptr)
+//        {
+//            return 0;
+//        }
+//        else
+//        {
+//            return 1 + max(maxDepth(root->left), maxDepth(root->right));
+//        }
+//    }
+//};
+
+class Solution {
 public:
-    int maxDepth(TreeNode* root) 
-    {
-        if (root == nullptr)
-        {
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
             return 0;
         }
-        else
-        {
-            return 1 + max(maxDepth(root->left), maxDepth(root->right));
-        }
+
+        int ans = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        do {
+            ++ans;
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                auto node = q.front();
+                q.pop();
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+        } while (!q.empty());
+
+        return ans;
     }
 };
