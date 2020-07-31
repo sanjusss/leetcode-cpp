1a69a44e007dff6d19df90b65adf2d9cc2198467
sanjusss <sanjusss@qq.com>
2020-07-31 19:42:52 +0800

    完成08.03

--- 程序员面试金典/08.03.hpp
+++ 程序员面试金典/08.03.hpp
@@ -4,30 +4,17 @@
 class Solution {
 public:
     int findMagicIndex(vector<int>& nums) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int mid;
-        int v;
-        while (left <= right) {
-            mid = (left + right) / 2;
-            v = nums[mid];
-            if (v == mid) {
-                return mid;
+        int i = 0;
+        while (i < nums.size()) {
+            if (i == nums[i]) {
+                return i;
             }
 
-            if (v < mid) {
-                if (v >= left && nums[v] == v) {
-                    return v;
-                }
-
-                left = mid + 1;
+            if (i < nums[i]) {
+                i = nums[i];
             }
             else {
-                if (v <= right && nums[v] == v) {
-                    return v;
-                }
-
-                right = mid - 1;
+                ++i;
             }
         }
 
