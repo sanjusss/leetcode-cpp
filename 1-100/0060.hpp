e02fcae58cfa7da3da8ddf22be6b26506bf999d2
sanjusss <sanjusss@qq.com>
2020-09-05 10:34:05 +0800

    优化60题。

--- 1-100/0060.hpp
+++ 1-100/0060.hpp
@@ -20,44 +20,68 @@
 //    }
 //};
 
-class Solution
-{
+//class Solution
+//{
+//public:
+//    string getPermutation(int n, int k)
+//    {
+//        vector<int> fn(n, 1);
+//        for (int i = 1; i < n; ++i)
+//        {
+//            fn[i] = i * fn[i - 1];
+//        }
+//
+//        vector<int> used(n + 1, 0);
+//
+//        string ans;
+//        --k;//让k从0开始计算。
+//        int r = n;
+//        while (--r >= 0)
+//        {
+//            int i = k / fn[r];
+//            k %= fn[r];
+//            for (int j = 1; j <= n; ++j)
+//            {
+//                if (used[j] == 1)
+//                {
+//                    continue;
+//                }
+//
+//                if (i == 0)
+//                {
+//                    used[j] = 1;
+//                    ans.push_back(j + '0');
+//                    break;
+//                }
+//                else
+//                {
+//                    --i;
+//                }
+//            }
+//        }
+//
+//        return ans;
+//    }
+//};
+
+class Solution {
 public:
-    string getPermutation(int n, int k)
-    {
-        vector<int> fn(n, 1);
-        for (int i = 1; i < n; ++i)
-        {
-            fn[i] = i * fn[i - 1];
+    string getPermutation(int n, int k) {
+        vector<int> cs(n + 1);
+        cs[0] = 1;
+        string temp;
+        for (int i = 1; i <= n; ++i) {
+            cs[i] = cs[i - 1] * i;
+            temp.push_back(i + '0');
         }
 
-        vector<int> used(n + 1, 0);
-
         string ans;
-        --k;//让k从0开始计算。
-        int r = n;
-        while (--r >= 0)
-        {
-            int i = k / fn[r];
-            k %= fn[r];
-            for (int j = 1; j <= n; ++j)
-            {
-                if (used[j] == 1)
-                {
-                    continue;
-                }
-
-                if (i == 0)
-                {
-                    used[j] = 1;
-                    ans.push_back(j + '0');
-                    break;
-                }
-                else
-                {
-                    --i;
-                }
-            }
+        --k;
+        for (int i = n; i > 0; --i) {
+            int j = k / cs[i - 1];
+            ans.push_back(temp[j]);
+            temp.erase(j, 1);
+            k %= cs[i - 1];
         }
 
         return ans;
