bd65cda1bf051bb4d24df2e82b0f4a7d0aea8201
sanjusss <sanjusss@qq.com>
2020-09-27 08:37:05 +0800

    增加235题另外一种思路

--- 201-300/0235.hpp
+++ 201-300/0235.hpp
@@ -1,6 +1,36 @@
 #pragma once
 #include "leetcode.h"
 
+//namespace _0235 {
+//    class Solution {
+//    public:
+//        TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//            if (root == nullptr) {
+//                return root;
+//            }
+//
+//            if (root == p || root == q) {
+//                return root;
+//            }
+//
+//            TreeNode* lr = lowestCommonAncestor(root->left, p, q);
+//            TreeNode* rr = lowestCommonAncestor(root->right, p, q);
+//            if (lr != nullptr && rr != nullptr) {
+//                return root;
+//            }
+//            else if (lr != nullptr) {
+//                return lr;
+//            }
+//            else if (rr != nullptr) {
+//                return rr;
+//            }
+//            else {
+//                return nullptr;
+//            }
+//        }
+//    };
+//}
+
 namespace _0235
 {
 
