de98bee868b00be0d4154d613799ad3111d98ded
sanjusss <sanjusss@qq.com>
2021-02-08 09:09:11 +0800

    增加978题另外一种思路

--- 0/900/970/978.cpp
+++ 0/900/970/978.cpp
@@ -1,35 +1,69 @@
 #include "leetcode.h"
 
+// class Solution {
+// public:
+//     int maxTurbulenceSize(vector<int>& arr) {
+//         int ans = 0;
+//         int left = 0;
+//         int right = 0;
+//         int n = arr.size();
+//         int sign = 0;
+//         while (right < n) {
+//             if (right > 0) {
+//                 int cur = 0;
+//                 if (arr[right] > arr[right - 1]) {
+//                     cur = 1;
+//                 }
+//                 else if (arr[right] < arr[right - 1]) {
+//                     cur = -1;
+//                 }
+
+//                 if (cur == 0) {
+//                     left = right;
+//                 }
+//                 else if (cur == sign){
+//                     left = right - 1;
+//                 }
+
+//                 sign = cur;
+//             }
+
+//             ++right;
+//             ans = max(ans, right - left);
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int ans = 0;
+        int ans = 1;
         int left = 0;
         int right = 0;
         int n = arr.size();
-        int sign = 0;
-        while (right < n) {
-            if (right > 0) {
-                int cur = 0;
-                if (arr[right] > arr[right - 1]) {
-                    cur = 1;
-                }
-                else if (arr[right] < arr[right - 1]) {
-                    cur = -1;
+        while (right < n - 1) {
+            if (left == right) {
+                if (arr[right] == arr[right + 1]) {
+                    ++left;
                 }
 
-                if (cur == 0) {
+                ++right;
+            }
+            else {
+                if (arr[right] > arr[right - 1] && arr[right] > arr[right + 1]) {
+                    ++right;
+                }
+                else if (arr[right] < arr[right - 1] && arr[right] < arr[right + 1]) {
+                    ++right;
+                }
+                else {
                     left = right;
                 }
-                else if (cur == sign){
-                    left = right - 1;
-                }
-
-                sign = cur;
             }
 
-            ++right;
-            ans = max(ans, right - left);
+            ans = max(ans, right - left + 1);
         }
 
         return ans;
