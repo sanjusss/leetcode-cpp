abe3b837e5e01d045580b267e87321985ecaf391
sanjusss <sanjusss@qq.com>
2021-09-18 08:29:00 +0800

    完成292

---
+++ 0/200/290/292.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: sanjusss
+ * @Date: 2021-09-18 08:28:43
+ * @LastEditors: sanjusss
+ * @LastEditTime: 2021-09-18 08:28:43
+ * @FilePath: \0\200\290\292.cpp
+ */
+#include "leetcode.h"
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4 != 0;
+    }
+};
