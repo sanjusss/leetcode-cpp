3cb58f9ceda4396827769982cecf444c8f0076e4
sanjusss <sanjusss@qq.com>
2022-02-17 11:29:57 +0800

    完成684

---
+++ 0/600/680/684_20220217.cpp
@@ -0,0 +1,69 @@
+/*
+ * @Author: sanjusss
+ * @Date: 2022-02-17 11:24:37
+ * @LastEditors: sanjusss
+ * @LastEditTime: 2022-02-17 11:26:58
+ * @FilePath: \0\600\680\684_20220217.cpp
+ */
+#include "leetcode.h"
+
+class UnionFind {
+public:
+    UnionFind(int n) : m_parent(n), m_size(n, 1), m_count(n) {
+        for (int i = 0; i < n; ++i) {
+            m_parent[i] = i;
+        }
+    }
+
+    int count() const {
+        return m_count;
+    }
+
+    int find(int i) {
+        if (m_parent[i] == i) {
+            return i;
+        }
+        else {
+            m_parent[i] = find(m_parent[i]);
+            return m_parent[i];
+        }
+    }
+
+    bool merge(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return false;
+        }
+
+        if (m_size[a] > m_size[b]) {
+            swap(a, b);
+        }
+
+        m_parent[a] = b;
+        m_size[b] += m_size[a];
+        --m_count;
+        return true;
+    }
+
+private:
+    vector<int> m_parent;
+    vector<int> m_size;
+    int m_count;
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        UnionFind uf(n + 1);
+        vector<int> ans;
+        for (auto& e : edges) {
+            if (!uf.merge(e[0], e[1])) {
+                swap(ans, e);
+            }
+        }
+
+        return ans;
+    }
+};
