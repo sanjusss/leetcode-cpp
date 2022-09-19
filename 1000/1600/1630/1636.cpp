bda646c30eb4ac72031a5689807d61f25106cf75
sanjusss <sanjusss@qq.com>
2022-09-19 14:25:35 +0800

    完成1636

--- 1000/1600/1630/1636.cpp
+++ 1000/1600/1630/1636.cpp
@@ -2,7 +2,7 @@
  * @Author: sanjusss
  * @Date: 2022-09-19 14:20:12
  * @LastEditors: sanjusss
- * @LastEditTime: 2022-09-19 14:22:53
+ * @LastEditTime: 2022-09-19 14:25:23
  * @FilePath: \1000\1600\1630\1636.cpp
  */
 #include "leetcode.h"
@@ -17,7 +17,7 @@ public:
 
         sort(nums.begin(), nums.end(), [&cnt](int a, int b) {
             if (cnt[a] == cnt[b]) {
-                return a >= b;
+                return a > b;
             }
             else {
                 return cnt[a] < cnt[b];
