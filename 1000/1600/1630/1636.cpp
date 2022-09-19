7415f13da81f34cb626cdb8dd5ba273cff9df053
sanjusss <sanjusss@qq.com>
2022-09-19 14:23:58 +0800

    完成1636

---
+++ 1000/1600/1630/1636.cpp
@@ -0,0 +1,29 @@
+/*
+ * @Author: sanjusss
+ * @Date: 2022-09-19 14:20:12
+ * @LastEditors: sanjusss
+ * @LastEditTime: 2022-09-19 14:22:53
+ * @FilePath: \1000\1600\1630\1636.cpp
+ */
+#include "leetcode.h"
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        for (int i : nums) {
+            ++cnt[i];
+        }
+
+        sort(nums.begin(), nums.end(), [&cnt](int a, int b) {
+            if (cnt[a] == cnt[b]) {
+                return a >= b;
+            }
+            else {
+                return cnt[a] < cnt[b];
+            }
+        });
+
+        return nums;
+    }
+};
