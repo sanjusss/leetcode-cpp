ae12e2b1e658b70e6b2d102b6779b126ee81db21
sanjusss <sanjusss@qq.com>
2022-11-17 08:52:43 +0800

    优化792

--- 0/700/790/792.cpp
+++ 0/700/790/792.cpp
@@ -2,38 +2,66 @@
  * @Author: sanjusss
  * @Date: 2022-11-17 08:37:20
  * @LastEditors: sanjusss
- * @LastEditTime: 2022-11-17 08:40:21
+ * @LastEditTime: 2022-11-17 08:51:40
  * @FilePath: \0\700\790\792.cpp
  */
 #include "leetcode.h"
 
+// class Solution {
+// public:
+//     int numMatchingSubseq(string s, vector<string>& words) {
+//         vector<vector<int>> pos(26);
+//         int n = s.size();
+//         for (int i = 0; i < n; ++i) {
+//             pos[s[i] - 'a'].push_back(i);
+//         }
+
+//         int ans = 0;
+//         for (auto& w : words) {
+//             int prev = -1;
+//             bool succuess = true;
+//             for (char c : w) {
+//                 int i = c - 'a';
+//                 auto p = upper_bound(pos[i].begin(), pos[i].end(), prev);
+//                 if (p == pos[i].end()) {
+//                     succuess = false;
+//                     break;
+//                 }
+//                 else {
+//                     prev = *p;
+//                 }
+//             }
+
+//             if (succuess) {
+//                 ++ans;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-        vector<vector<int>> pos(26);
-        int n = s.size();
-        for (int i = 0; i < n; ++i) {
-            pos[s[i] - 'a'].push_back(i);
+        vector<queue<pair<int, int>>> qs(26);
+        int m = words.size();
+        for (int i = 0; i < m; ++i) {
+            qs[words[i][0] - 'a'].emplace(i, 0);
         }
 
         int ans = 0;
-        for (auto& w : words) {
-            int prev = -1;
-            bool succuess = true;
-            for (char c : w) {
-                int i = c - 'a';
-                auto p = upper_bound(pos[i].begin(), pos[i].end(), prev);
-                if (p == pos[i].end()) {
-                    succuess = false;
-                    break;
+        for (char c : s) {
+            auto& q = qs[c - 'a'];
+            for (int qi = q.size(); qi > 0; --qi) {
+                auto [i, j] = q.front();
+                q.pop();
+                if (++j == words[i].size()) {
+                    ++ans;
                 }
                 else {
-                    prev = *p;
+                    qs[words[i][j] - 'a'].emplace(i, j);
                 }
-            }
-
-            if (succuess) {
-                ++ans;
             }
         }
 
