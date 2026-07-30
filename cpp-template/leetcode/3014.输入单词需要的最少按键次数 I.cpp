a25b05ca5fdac3cdc65aa1f89ef7d1b16da04155
sanjusss <sanjusss@qq.com>
2026-07-30 14:40:12 +0800

    完成2026年7月30日每日一题

---
+++ cpp-template/leetcode/3014.输入单词需要的最少按键次数 I.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=3014 lang=cpp
+ * @lcpr version=30404
+ *
+ * [3014] 输入单词需要的最少按键次数 I
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int cnts[26] = {};
+        for (char c : word) {
+            cnts[c - 'a'] += 1;
+        }
+
+        sort(cnts, cnts + 26);
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            ans += cnts[26 - 1 - i] * ((i + 8) / 8);
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "abcde"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "xycdefghij"\n
+// @lcpr case=end
+
+ */
+
